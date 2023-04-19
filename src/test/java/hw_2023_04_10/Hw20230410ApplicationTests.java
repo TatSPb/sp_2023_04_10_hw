620a2a63040be7cf94cf2f01ce8f5972ce3359c4
TatSPb <t-sverchkova@yandex.ru>
2023-04-19 22:05:24 +0300

    Commit-1

---
+++ src/test/java/hw_2023_04_10/Hw20230410ApplicationTests.java
@@ -0,0 +1,13 @@
+package hw_2023_04_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20230410ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
